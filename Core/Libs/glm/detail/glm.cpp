6710346e8e17f85467933e6caf206c235df069ee
Brett Constantinoff <constantinoffbrett@gmail.com>
2022-05-16 16:42:10 -0600

    fixes include path

--- Core/Libs/glm/detail/glm.cpp
+++ Core/Libs/glm/detail/glm.cpp
@@ -4,12 +4,18 @@
 #ifndef GLM_ENABLE_EXPERIMENTAL
 #define GLM_ENABLE_EXPERIMENTAL
 #endif
-#include <glm/gtx/dual_quaternion.hpp>
-#include <glm/gtc/vec1.hpp>
-#include <glm/gtc/quaternion.hpp>
-#include <glm/ext/scalar_int_sized.hpp>
-#include <glm/ext/scalar_uint_sized.hpp>
-#include <glm/glm.hpp>
+//#include <glm/gtx/dual_quaternion.hpp>
+#include "../gtx/dual_quaternion.hpp"
+#include "../gtc/quaternion.hpp"
+#include "../gtc/vec1.hpp"
+#include "../ext/scalar_int_sized.hpp"
+#include "../ext/scalar_uint_sized.hpp"
+#include "../glm.hpp"
+//#include <glm/gtc/vec1.hpp>
+//#include <glm/gtc/quaternion.hpp>
+//#include <glm/ext/scalar_int_sized.hpp>
+#//include <glm/ext/scalar_uint_sized.hpp>
+//#include <glm/glm.hpp>
 
 namespace glm
 {
